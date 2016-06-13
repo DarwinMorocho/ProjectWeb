53949be61b0a8c61bcbc092793b7f576703deef1
Jonathan Morocho <jonita2005@gmail.com>
2016-06-13 01:07:07 -0500

    correcciones menores a detalle factura

--- src/controlador/GuardaDetalleTemporal.java
+++ src/controlador/GuardaDetalleTemporal.java
@@ -1,12 +1,14 @@
 package controlador;
 
 import modelo.DetalleFactura;
+import modelo.Factura;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GuardaDetalleTemporal {
 	public static List<DetalleFactura> detallesFactura = new ArrayList<DetalleFactura>();
+	public static List<Factura> datosFactura = new ArrayList<Factura>();
 
 
 }
